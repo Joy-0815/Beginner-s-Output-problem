ad4e78403b3125fffc8f40b09c1f3abdb6484c61
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-07 08:43:29 +0000

    wow

---
+++ MadLibs.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+adjective = input("Enter an adjective:")
+verb = input("Enter a verb:")
+food = input("Enter a food:")
+vehicle = input("Enter a vehicle:")
+
+print(name,"is a",adjective,"person. They like to",verb,". Their favourite food is",food,". They travel to school by a",vehicle)
