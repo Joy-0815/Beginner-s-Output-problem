5b896768ab8573e1e2b01dd3ba8cb7d4a58353ac
Jocelyn <160691921+Joy-0815@users.noreply.github.com>
2024-05-07 08:23:46 +0000

    shocking

--- yrday.py
+++ yrday.py
@@ -2,4 +2,6 @@ years = int(input("How old are you?"))
 days = 365
 sum = years*days
 
-print("You are",sum,"days old.")
+yrsold = (36500-sum)
+
+print("You are",sum,"days old. You'll be 100 years old in",yrsold,"days!")
